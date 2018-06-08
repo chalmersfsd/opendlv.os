e7e456d2ffd6362d8676ff9ee1514aba81212ddc
Ziwei Huang <z.huangcsee@gmail.com>
2018-06-08 14:38:07 +0200

    change root path

--- x86/get.sh
+++ x86/get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_URL=https://raw.githubusercontent.com/chalmers-revere/opendlv.os/master/x86
+ROOT_URL=https://raw.githubusercontent.com/cfsd/opendlv.os/master/x86
 
 wget ${ROOT_URL}/{install,install-conf,install-chroot,install-post}.sh
 
